68f4d9e83283fd07763a52819a9bd98c4da8da35
tataglarasati11 <60909380+tataglarasati11@users.noreply.github.com>
2020-04-21 19:54:48 +0700

    Update program lingkaran dan tabung.cpp

--- program lingkaran dan tabung.cpp
+++ program lingkaran dan tabung.cpp
@@ -15,25 +15,24 @@ int main()
 	char ulang;
 
 do{
-		header ();
-	    cout << "Pilihan:" << endl;
-	    cout << "1. Lingkaran "<< endl ;
-		cout << "2. Tabung "<< endl;
-		cout << "Masukkan piihan anda : ";
-	    cin	 >> pilih;
-	    system("cls");
-	    switch (pilih){
-	    	case 1 :
-				Lingkaran ();
-			break ;
+	header ();
+	cout << "Pilihan:" << endl;
+	cout << "1. Lingkaran "<< endl ;
+	cout << "2. Tabung "<< endl;
+	cout << "Masukkan piihan anda : ";
+	cin >> pilih;
+	system("cls");
+	switch (pilih){
+		case 1 :
+			Lingkaran ();
+		break ;
 			
-			case 2 :  
-				Tabung ();
-			break ;
-			default:
-				cout<<"Pilihan yang anda inginkan tidak ditemukan"<<endl;
-			break;
-			
+		case 2 :  
+			Tabung ();
+		break ;
+		default:
+			cout<<"Pilihan yang anda inginkan tidak ditemukan"<<endl;
+		break;	
 		}
 		cout << "apakaah ingin menghitung lagi ? "<<endl ;
 		cout << "Y/T"<<endl;
@@ -46,49 +45,48 @@ do{
 	cout << "=============================="<<endl;	
 	cout << "Tatag Indah Larasati"<<endl;
 	cout << " D04217025"<<endl;
-    cout << "=============================="<<endl;	
+   	cout << "=============================="<<endl;	
 }
-	
-	void header (){
-		cout << "==================================="<< endl;
-		cout << "Program menghitung lingkaran dan tabung"<< endl;
-		cout << "==================================="<< endl;
-	}
+void header (){
+	cout << "==================================="<< endl;
+	cout << "Program menghitung lingkaran dan tabung"<< endl;
+	cout << "==================================="<< endl;
+}
 		
-	void Lingkaran () {
-		int r;
-		int pilih;
-		float phi = 3.14;
-		float luas ;
-		float keliling ;
-			  	header ();
-	    		cout << "1. Keliling " 						<< endl ;
-				cout << "2. Luas " 							<< endl ;
-				cout << "=================================="<< endl ;
-	    		cout << "masukkan piihan anda : ";
-	    		cin >> pilih;
-	    		system("cls");
-	    		switch (pilih) {
-	    			case 1 : 
-		    			header();
-				   		cout << "Mencari Keliling lingkaran "<<endl ;
-				   		cout << " Masukkan nilai jari-jari : ";
-				   		cin >> r;
-				   		keliling = 2*phi*r;
-				   		cout << "keliling lingkara adalah :" <<keliling<<"\n";
-					break;
-	    			case 2 :
-						header();
-				   		cout << "Mencari Luas lingkaran"<<endl ;
-				   		cout << " Masukkan nilai jari-jari : ";
-				   		cin >> r;
-				   		luas = phi*r*r;
-				   		cout << "Luas lingkaran adalah :" <<luas<<"\n";
-					break;	
-					default:
-						cout<<"Pilihan yang anda inginkan tidak ditemukan"<<endl;
-					break;	
-				}
+void Lingkaran () {
+	int r;
+	int pilih;
+	float phi = 3.14;
+	float luas ;
+	float keliling ;
+	header ();
+	cout << "1. Keliling " 				<< endl ;
+	cout << "2. Luas " 				<< endl ;
+	cout << "======================================"<< endl ;
+	cout << "masukkan piihan anda : ";
+	cin >> pilih;
+	system("cls");
+	switch (pilih) {
+		case 1 : 
+			header();
+			cout << "Mencari Keliling lingkaran "<<endl ;
+			cout << " Masukkan nilai jari-jari : ";
+			cin >> r;
+			keliling = 2*phi*r;
+			cout << "keliling lingkara adalah :" <<keliling<<"\n";
+		break;
+	    	case 2 :
+			header();
+			cout << "Mencari Luas lingkaran"<<endl ;
+			cout << " Masukkan nilai jari-jari : ";
+			cin >> r;
+			luas = phi*r*r;
+			cout << "Luas lingkaran adalah :" <<luas<<"\n";
+		break;	
+		default:
+			cout<<"Pilihan yang anda inginkan tidak ditemukan"<<endl;
+		break;	
+		}
 		
 	}
 	
@@ -99,38 +97,37 @@ void Tabung (){
 	float phi = 3.14;
 	float Lp;
 	float volume;
-	
-				header ();
-				cout << "1. Luas Permukaan " 				<< endl ;
-				cout << "2. Volume " 						<< endl ;
-				cout << "=================================="<< endl ;
-				cout << "masukkan piihan anda : ";
-	    		cin >> pilih;
-	    		system("cls");
-	    		switch (pilih) {
-	    			case 1 : 
-		    			header ();
-				   		cout << "Mencari luas permukaan tabung"<<endl ;
-				   		cout << " Masukkan nilai jari-jari : ";
-				   		cin >> r;
-				   		cout << " Masukkan nilai tinggi : ";
-				   		cin >> t;
-			   			Lp = 2*phi*r*(r+t) ;
-						cout << "luas permukaan tabung adalah :" <<Lp<<"\n";
-	    			break ;
-	    			case 2 :	
-						header ();
-			    		cout << "Mencari volume tabung"<<endl ;
-				   		cout << " Masukkan nilai jari-jari : ";
-				   		cin >> r;
-				 		cout << " Masukkan nilai tinggi : ";
-				   		cin >> t;
-				   		volume = phi*r*r*t;
-				   		cout << "volume tabung adalah :" <<volume<<"\n";	
-					break;
-					default:
-						cout<<"Pilihan yang anda inginkan tidak ditemukan"<<endl;
-					break;		
-				}
+	header ();
+	cout << "1. Luas Permukaan " 			<< endl ;
+	cout << "2. Volume " 				<< endl ;
+	cout << "======================================"<< endl ;
+	cout << "masukkan piihan anda : ";
+	cin >> pilih;
+	system("cls");
+	switch (pilih) {
+		case 1 : 
+			header ();
+			cout << "Mencari luas permukaan tabung"<<endl ;
+			cout << " Masukkan nilai jari-jari : ";
+			cin >> r;
+			cout << " Masukkan nilai tinggi : ";
+			cin >> t;
+			Lp = 2*phi*r*(r+t) ;
+			cout << "luas permukaan tabung adalah :" <<Lp<<"\n";
+	    	break ;
+	    	case 2 :	
+			header ();
+			cout << "Mencari volume tabung"<<endl ;
+			cout << " Masukkan nilai jari-jari : ";
+			cin >> r;
+			cout << " Masukkan nilai tinggi : ";
+			cin >> t;
+			volume = phi*r*r*t;
+			cout << "volume tabung adalah :" <<volume<<"\n";	
+		break;
+		default:
+			cout<<"Pilihan yang anda inginkan tidak ditemukan"<<endl;
+		break;		
+	}
 }
 
